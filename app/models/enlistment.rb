6a9934dc45a0fc6a7e0088cf19989745f91565d1
Harish Natarajan <harish.natarajan@imaginea.com>
2015-03-13 15:15:23 +0530

    Replaced sortable with scopes

--- app/models/enlistment.rb
+++ app/models/enlistment.rb
@@ -4,6 +4,10 @@ class Enlistment < ActiveRecord::Base
 
   acts_as_editable editable_attributes: [:ignore]
   acts_as_protected parent: :project
+  scope :sort_by_url, ->{ order('projects.name, repositories.url, repositories.module_name').sort}
+  scope :sort_by_project, ->{order('projects.name, repositories.url, repositories.module_name').sort }
+  scope :sort_by_type, ->{order('repositories.type, repositories.url, repositories.module_name').sort }
+  scope :sort_by_module_name, ->{order('repositories.module_name, repositories.url').sort }
 
   class << self
     def enlist_project_in_repository(editor_account, project, repository, ignore = nil)
@@ -17,4 +21,22 @@ class Enlistment < ActiveRecord::Base
       enlistment.reload
     end
   end
+
+  delegate 'failed?', :to => :repository
+
+=begin  
+  def explain_yourself(edit) # not required as i didn't find act_as editable with explaination
+  end
+
+  def ensure_forge_and_job # i ahven't seen any after_save calbacks here 
+  end
+
+  def self.with_url_like(like) # not used anywhere
+  end
+
+  def analysis_sloc_set #CodeSet model not found
+  end
+  def ignore_examples not used anywhere
+  end
+=end  
 end
